746318883f11028c9e03a979db3dc834003a0c9d
Rohit Chakraborty <99498195+RohitChakraborty2k02@users.noreply.github.com>
2024-06-03 00:03:07 +0530

    Create Prob 273.java

---
+++ Prob 273.java
@@ -0,0 +1,20 @@
+class Solution {
+    private final String[] belowTen = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    private final String[] belowTwenty = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] belowHundred = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return helper(num).trim();
+    }
+
+    private String helper(int num) {
+        if (num < 10) return belowTen[num];
+        if (num < 20) return belowTwenty[num - 10];
+        if (num < 100) return belowHundred[num / 10] + (num % 10 != 0 ? " " + helper(num % 10) : "");
+        if (num < 1000) return helper(num / 100) + " Hundred" + (num % 100 != 0 ? " " + helper(num % 100) : "");
+        if (num < 1000000) return helper(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + helper(num % 1000) : "");
+        if (num < 1000000000) return helper(num / 1000000) + " Million" + (num % 1000000 != 0 ? " " + helper(num % 1000000) : "");
+        return helper(num / 1000000000) + " Billion" + (num % 1000000000 != 0 ? " " + helper(num % 1000000000) : "");
+    }
+}
