2c3df149833003f71ec17406e5badd2e55bc4c8c
Rohit Chakraborty <99498195+RohitChakraborty2k02@users.noreply.github.com>
2024-07-05 02:29:24 +0530

    Create Prob 21.java

---
+++ Prob 21.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(-1);
+        ListNode demo = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                demo.next = list2;
+                list2 = list2.next;
+            } else {
+                demo.next = list1;
+                list1 = list1.next;
+            }
+            demo = demo.next;
+
+        }
+
+        if (list1 == null) {
+            demo.next = list2;
+        }
+        if (list2 == null) {
+            demo.next = list1;
+        }
+
+        return temp.next;
+    }
+}
